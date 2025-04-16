9457cbbf6324c3651f12613285a5bcbf01c62224
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-04-16 07:54:09 +0700

    Use tam.mml (TAM) With (In) R Software

---
+++ tam.mml/tam.mml.R
@@ -0,0 +1,15 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Test Analysis Modules: Marginal Maximum Likelihood Estimation Use tam.mml (TAM) With (In) R Software
+install.packages("TAM")
+library("TAM")
+tam.mml = read.csv("https://raw.githubusercontent.com/timbulwidodostp/tam.mml/main/tam.mml/tam.mml.csv",sep = ";")
+# Estimation Test Analysis Modules: Marginal Maximum Likelihood Estimation Use tam.mml (TAM) With (In) R Software
+tam.mml <- TAM::tam.mml(resp=tam.mml)
+tam.mml
+# Test Analysis Modules: Marginal Maximum Likelihood Estimation Use tam.mml (TAM) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
